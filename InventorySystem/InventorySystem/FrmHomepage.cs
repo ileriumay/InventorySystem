710c392d0018ae3640231b142f7ef361e10e37d7
efeyiit <efeyiitkucuk@gmail.com>
2025-01-02 23:25:47 +0300

    Add files via upload
    Integrated backend functionality into the existing frontend code. Added necessary API routes, database connections, and logic to handle data processing and management. Ensured smooth communication between frontend and backend components, allowing dynamic content rendering based on data from the server. Updated the repository with the necessary changes for full-stack functionality.

---
+++ InventorySystem/InventorySystem/FrmHomepage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventorySystem
+{
+    public partial class FrmHomepage : Form
+    {
+        public FrmHomepage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnManageItems_Click(object sender, EventArgs e)
+        {
+            var manageItems = new FrmManageItems();
+            manageItems.Show();
+
+        }
+
+        private void btnListStocks_Click(object sender, EventArgs e)
+        {
+            var listStocks = new FrmListStocks();
+            listStocks.Show();
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Application will be close. Are you sure?", "Sure? ", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+
+        }
+    }
+}
